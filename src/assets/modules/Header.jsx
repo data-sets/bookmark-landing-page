babe25026f4f74ff3418d42caba9ea8be77185a7
Jade Banares <jade.arinal.banares@gmail.com>
2023-05-10 10:48:05 +0800

    added: initial markup for featured section

--- src/assets/modules/Header.jsx
+++ src/assets/modules/Header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Monitor from "../images/illustration-hero.svg";
-
+import Illustration from "../images/illustration-hero.svg";
+import Illustration1 from "../images/illustration-features-tab-1.svg";
 export default class Navigation extends Component {
   render() {
     return (
@@ -20,11 +20,59 @@ export default class Navigation extends Component {
               </div>
             </div>
             <div className="header-section__illustration">
-              <img src={Monitor} alt="" />
+              <img src={Illustration} alt="" />
               <div className="shape"></div>
             </div>
           </div>
         </header>
+        <section className="features-section">
+          <div className="features-section__container">
+            <div>
+              <h2>Features</h2>
+              <p>
+                Our aim is to make it quick and easy for you to access your
+                favourite websites. Your bookmarks sync between your devices so
+                you can access them on the go.
+              </p>
+            </div>
+            <div className="features-section__navigation">
+              <ul>
+                <li>
+                  <button>
+                    <h3>Simple Bookmarking</h3>
+                  </button>
+                </li>
+                <li>
+                  <button>
+                    <h3>Speedy Searching</h3>
+                  </button>
+                </li>
+                <li>
+                  <button>
+                    <h3>Easy Sharing</h3>
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <div className="features-section__offers">
+              <div>
+                <div>
+                  <img src={Illustration1} alt="Illustration" />
+                </div>
+                <div></div>
+              </div>
+              <div>
+                <h3>Bookmark in one click</h3>
+                <p>
+                  Organize your bookmarks however you like. Our simple
+                  drag-and-drop interface gives you complete control over how
+                  you manage your favourite sites.
+                </p>
+                <button>More Info</button>
+              </div>
+            </div>
+          </div>
+        </section>
       </>
     );
   }
